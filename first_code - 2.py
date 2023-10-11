e1fb21abdf4bb8a86a3230c58fef3bc6de92aecd
unmaosafsdgsol <ojh200103@gmail.com>
2023-10-11 15:16:01 +0900

    ㅇ로옹ㄹ

---
+++ first_code - 2.py
@@ -0,0 +1,2 @@
+print ("wellcome to mte world")
+print ("뭉탱이월드에 오신걸 환영합니다.")
---
+++ first_code - 복사본.py
@@ -0,0 +1,7 @@
+print ("wellcome to mte world")
+print ("사각형의 넓이 구하기")
+
+
+def calc_area(x,y):
+    result =x*y
+    return result
