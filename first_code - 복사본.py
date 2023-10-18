f6d50bf28981678c07829c415ee373624b409f74
unmaosafsdgsol <ojh200103@gmail.com>
2023-10-18 14:50:04 +0900

    gdsds

--- first_code - 복사본.py
+++ first_code - 복사본.py
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-print ("wellcome to mte world")
-print ("사각형의 넓이 구하기")
-
-
-def calc_area(x,y):
-    result =x*y
-    return result
-=======
 print("hello world")
 print("1부터 10까지 더하세요")
 total = 0
@@ -15,4 +6,3 @@ for i in range(1, 11):
     total += i
 
 print("1부터 10까지의 합:", total)
->>>>>>> feature/calc_area
