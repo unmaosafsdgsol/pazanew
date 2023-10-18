97d5c02fb83d2e5fe23442a637034f29d9df05a6
unmaosafsdgsol <ojh200103@gmail.com>
2023-10-18 14:51:05 +0900

    Revert "gdsds"
    This reverts commit f6d50bf28981678c07829c415ee373624b409f74.

--- first_code - 2.py
+++ first_code - 2.py
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
+print ("wellcome to mte world")
+print ("뭉탱이월드에 오신걸 환영합니다.")
+=======
 print("wellcome to mte world")
 print("사각형의 넓이 구하기")
 
 def calc_area(x,y):
     result =x*y
     return result
+>>>>>>> feature/calc_area
